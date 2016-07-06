dcef70398788f3e408ab7d06c282c6b7956c8cc0
airetnes <hukutuk35@gmail.com>
2016-07-06 18:19:15 +0700

    create project

---
+++ resources/views/layouts/_nav.blade.php
@@ -0,0 +1,37 @@
+<div class="col-md-12">
+    <div class="row">
+        <a id="touch-menu" class="mobile-menu" href="#"><i class="fa fa-navicon"></i>Меню</a>
+
+        <nav id="glavnav" class="signuplight">
+            <ul class="menu1">
+                <li><a href="#"><i class="fa fa-medkit"></i>Услуги и стоимость</a>
+                    <ul class="sub-menu">
+                        <li><a href="#">Описание МРТ и КТ</a></li>
+                        <li><a href="#">Видеоконсультация</a></li>
+                        <li><a href="#">Экстренная консультация</a></li>
+                        <li><a href="#">Услуги и стоимость</a></li>
+                    </ul>
+                </li>
+                <li><a href="#"><i class="fa fa-user-md"></i>Врачи</a>
+                    <ul class="sub-menu">
+                        <li><a href="#">Клиники-партнеры</a></li>
+                        <li><a href="#">Выбор специалиста</a></li>
+                        <li><a href="#">Лучевая диагностика</a></li>
+                    </ul>
+                </li>
+                <li><a href="adult"><i class="fa fa-stethoscope"></i>Специализации</a>
+                    <ul class="sub-menu">
+                        <li><a href="adult">Взрослые</a></li>
+                        <li><a href="kids">Дети</a></li>
+                    </ul>
+                </li>
+                <li><a href="contact"><i class="fa fa-map-marker"></i>Контакты</a></li>
+                <li><a href="user?act=index"><i class="fa fa-user"></i>Личный кабинет</a></li>
+            </ul>
+
+            <div class="top_contacts"><i class="fa fa-phone faa-ring animated faa-slow hidden-xs"></i> <a
+                        href="tel:+79999999999">+7 (999) 999 9999</a></div>
+        </nav>
+
+    </div>
+</div>
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <!-- Required meta tags always come first -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>@yield('title') | Medicato</title>
+
+    <link rel="stylesheet" href="{{URL::asset('assets/css/reset.css')}}">
+    <link rel="stylesheet" href="{{URL::asset('assets/css/bootstrap-grid-3.3.1.min.css')}}">
+    <link rel="stylesheet" href="{{URL::asset('assets/css/main.css')}}">
+    <link rel="stylesheet" href="{{URL::asset('assets/css/media.css')}}"/>
+    <link rel="stylesheet" href="{{URL::asset('assets/css/fonts.css')}}">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="{{URL::asset('assets/css/font-awesome-animation.min.css')}}">
+    <link rel="stylesheet" href="{{URL::asset('assets/css/flag-icon.css')}}">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+          integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+    <script type="text/javascript" src="{{URL::asset('assets/js/modernizr.js')}}"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+</head>
+<body>
+<header class="top_header">
+    <div class="header_topline">
+        <div class="container">
+            <div class="col-md-12">
+                <div class="row">
+
+                    <div class="top_links">
+                        <a href="/" class="logo">Medicato</a>
+                    </div>
+
+                    <div class="top_links hidden-xs" style="padding: 13px 0px;">
+                        <b>Lorem ipsum dolor.</b>
+                    </div>
+
+                    {{--<ul class="language_bar_chooser">
+                        @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
+                            <li>
+                                <a rel="alternate" hreflang="{{$localeCode}}" href="{{LaravelLocalization::getLocalizedURL($localeCode) }}">
+                                    {{ $properties['native'] }}
+                                </a>
+                            </li>
+                        @endforeach
+                    </ul>--}}
+
+                    <div class="soc_buttons">
+                        @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
+                            <?php $flag = ($localeCode == 'en') ? 'um' : $localeCode; ?>
+                            <a rel="alternate" hreflang="{{$localeCode}}" href="{{LaravelLocalization::getLocalizedURL($localeCode) }}">
+                                <span class="flag-icon flag-icon-{{$flag}} flag-icon-background"></span>
+                            </a>
+                        @endforeach
+                        {{--<a href="#" target="_blank"><i class="fa fa-vk"></i></a>--}}
+                        {{--<a href="#" target="_blank"><i class="fa fa-facebook-square"></i></a>--}}
+                        {{--<a href="#" target="_blank"><i class="fa fa-twitter"></i></a>--}}
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        @include('layouts._nav')
+    </div>
+</header>
+
+
+
+@yield('content')
+
+
+<div class="container footer">
+    <div class="row">
+        <div class="col-md-4 col-sm-4 text-muted">
+            <p><a href="/" class="logo" style="color:#333;">Medicato</a></p>
+            <p>
+                © ООО «Рога и Копыта» 2016
+                Используя данный вебсайт, вы&nbsp;подтверждаете согласие с
+                <a href="#">правилами использования</a>.
+            </p>
+        </div>
+
+        <div class="col-md-2 col-sm-4">
+            <div class="signuplight">
+                <h4 class="text-muted">Medicato</h4>
+                <ul>
+                    <li><a href="user?act=index">Личный кабинет</a></li>
+                    <li><a href="#">Услуги и стоимость</a></li>
+                    <li><a href="adult">Специализации</a></li>
+                    <li><a href="#">Врачи</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="col-md-2 col-sm-4">
+            <div>
+                <h4 class="text-muted">О компании</h4>
+                <ul>
+                    <li><a href="contact">Контакты</a></li>
+                    <li><a href="#">Договора</a></li>
+                    <li><a href="#">Отзывы</a></li>
+                    <li><a href="#">Оферта</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="col-md-2 col-sm-4">
+            <div>
+                <h4 class="text-muted">Помощь</h4>
+                <ul>
+                    <li><a href="#">Безопасность платежей</a></li>
+                    <li><a href="#">Инфо для клиента</a></li>
+                    <li><a href="#">Задать вопрос</a></li>
+                    <li><a href="#">Правила</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="col-md-2 col-sm-4 hidden-md hidden-lg">
+            <div>
+                <h4 class="text-muted">Контакты</h4>
+                <ul>
+                    <li>город Кемерово</li>
+                    <li>пр. Ленина 19б/2</li>
+                    <li><a href="#">+7 (999) 999 9999</a></li>
+                    <li><a href="mailto:qwerty@qwerty.ru">qwerty@qwerty.ru</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="col-md-2 col-sm-4">
+            <div>
+                <h4 class="text-muted">Присоединяйся</h4>
+                <div class="soc_buttons">
+                    <a href="#" target="_blank"><i class="fa fa-vk"></i></a>
+                    <a href="#" target="_blank"><i class="fa fa-facebook-square"></i></a>
+                    <a href="#" target="_blank"><i class="fa fa-twitter"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row text-muted">
+        <div class="col-md-9">
+            <span>Мы принимаем к оплате</span>
+            <img src="{{URL::asset('assets/img/visa.png')}}" alt="Visa footer">
+            <img src="{{URL::asset('assets/img/mastercard.png')}}" alt="Master card footer">
+        </div>
+        <div class="col-md-3 copyright">
+            <span>Разработка сайта <a href="http://best-designs.ru">best-designs.ru</a></span>
+        </div>
+    </div>
+
+</div>
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+<script type="text/javascript" src="{{URL::asset('assets/js/main-navigation.js')}}"></script>
+<script type="text/javascript" src="{{URL::asset('assets/js/main.js')}}"></script>
+
+<script>
+    $(function () {
+        $('[data-toggle="popover"]').popover({
+            trigger: 'hover',
+            placement: 'bottom'
+        })
+    })
+</script>
+</body>
+</html>
